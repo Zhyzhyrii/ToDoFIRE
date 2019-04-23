93db5107d0478c8735935e94627f23a461829b78
izhizhirii <izhizhirii@gmail.com>
2019-04-23 12:36:19 +0300

    Initial Commit

---
+++ ToDoFIRE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoFIRE
+//
+//  Created by Игорь on 4/23/19.
+//  Copyright © 2019 Igor Zhyzhyrii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
