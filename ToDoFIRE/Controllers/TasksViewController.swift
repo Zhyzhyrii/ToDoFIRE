da7c0de9698e97f77b803ec0ce4abeb9bd68c095
izhizhirii <izhizhirii@gmail.com>
2019-04-25 12:58:32 +0300

    Add authentication functionality

--- ToDoFIRE/Controllers/TasksViewController.swift
+++ ToDoFIRE/Controllers/TasksViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
@@ -26,6 +27,15 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
 
     @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func signOutTapped(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print(error.localizedDescription)
+        }
+        dismiss(animated: true, completion: nil)
     }
     
 }
